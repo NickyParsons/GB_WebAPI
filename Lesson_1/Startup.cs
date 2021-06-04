44d3ea410f1c51022693d9a20ef912c9f882225a
NickyParsons <78694120+NickyParsons@users.noreply.github.com>
2021-06-04 20:10:18 +0300

    Lesson 1 commit 2
    fix

--- Lesson_1/Startup.cs
+++ Lesson_1/Startup.cs
@@ -26,7 +26,7 @@ namespace Lesson_1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<WeatherForecastList>();
+            services.AddSingleton<WeatherForecastHolder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
